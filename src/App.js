5b003f1723d35306df4cea2a2ca779b366c30d98
Cameron <sicklecj@gmail.com>
2023-02-08 15:40:48 -0600

    update style add netflix clone change titles

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import Gallery from './components/Gallery';
 function App() {
   const [categories] = useState([
     {
-      name: 'Bio',
-      description: 'Welcome to my portfolio! This application is built with REACT. Click the photos below to learn more about me. You may further explore my portfolio by clicking the titles and icons on the navigation bar. Be sure to visit my LinkedIn, Github, and Youtube.',
+      name: 'About Me',
+      description: 'Former Insurance Agent Turned Developer',
     },
     {
-      name: 'Experience',
-      description: 'Click the photos below to learn about applications I worked on. Please checkout my GitHub and Youtube to see more examples.',
+      name: 'Projects',
+      description: 'Click the photos below to view my projects. You can visit my GitHub for additional information.',
     },
     { name: 'Resume', description: 'Click the photos below to see more information about my work history, relevant experience, and certifications.' }
   ]);
--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -9,46 +9,54 @@ const PhotoList = ({ category }) => {
 
         {
             name: 'Run Buddy',
-            category: 'Experience',
+            category: 'Projects',
             description:
                 'Run Buddy is a mock website that offers fitness training services. This was my first learning project in the University of Wisconsin Coding Bootcamp. HTML is the primary language used to develop this website. Click the image to visit Run Buddy in a new browser.',
             link: 'https://cameronsickler.github.io/run-buddy/'
         },
         {
             name: 'Meal Maker',
-            category: 'Experience',
+            category: 'Projects',
             description:
                 'Meal Maker is a mobile first application created as a tool to help users find recipes by inputing food ingredients and returning recipes containing those ingredients to the user. This was our first group project in the University of Wisconsin Full Stack Coding Bootcamp. Materialize CSS, Spoonacular, Edamam, and Javascript are primary resources used to develop this application. Click the image to visit Meal Maker in a new browser.',
             link: 'https://patrickarcher414.github.io/meal-maker/'
         },
         {
             name: 'Coding N Kobolds',
-            category: 'Experience',
+            category: 'Projects',
             description:
                 'Coding N Kobolds fantasy avatar creator application that allows the user to create, edit, and save custom avatars. This was our second group project in the University of Wisconsin Full Stack Coding Bootcamp. Node Package Manager (NPM), My Structured Query Language (MYSQL), Sequelize, Heroku, Handlebars, Bootstrap CSS, and Javascript are the primary resources used to develop this application. Click the image to visit Coding N Kobolds in a new browser.',
             link: 'https://codingnkobolds.herokuapp.com/'
         },
         {
             name: 'Gather The Magic',
-            category: 'Experience',
+            category: 'Projects',
             description:
                 'Gather the Magic is a full stack single page application (SPA) that allows users to view Magic the Gathering cards. This was our last group project in the University of Wisconsin Full Stack Coding Bootcamp. MongoDB, GraphQL API, Express.js, Node.js, REACT, Heroku, and Javascript are the primary resources used to develop this application. Click the image to visit Gather The Magic in a new browser. My apologizes if the application is not available, I am working with my team to fix the issue.',
             link: 'https://gather-the-magic.herokuapp.com/'
-        }, {
+        },
+        {
+            name: 'Netflix Clone',
+            category: 'Projects',
+            description:
+                'Netflix Clone is a project I am actively working on to deepen my understanding of REACT. When it is finished, it should closely resemble the Netflix website.',
+            link: 'https://github.com/CameronSickler/netflix-clone/'
+        },
+        {
             name: 'Cameron Sickler',
-            category: 'Bio',
+            category: 'About Me',
             description:
                 "My name is Cameron Sickler and I am a recent graduate of the University of Wisconsin Extended Campuses Full Stack Computer Coding Bootcamp. Prior to coding I obtained a Bachelor's degree in Communication from the University of Wisconsin Eau Claire and an Associate Degree from the University of Wisconsin Fox Valley."
 
         }, {
             name: 'Hobbies',
-            category: 'Bio',
+            category: 'About Me',
             description:
                 'A few of my favorite activities include cycling, sandpit volleyball leagues, bringing my dog to the dog park, and causally learning spoken languages (currently working on Italian).'
 
         }, {
             name: 'Personal Life',
-            category: 'Bio',
+            category: 'About Me',
             description:
                 'I currently reside in Madison Wisconsin and have been living here since 2021. I try to explore a few  local events every month if I can! Living here with me is my dog and two cats.'
 
